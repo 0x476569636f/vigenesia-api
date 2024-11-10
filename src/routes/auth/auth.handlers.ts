7ebcf62c1163f51b2d256bdbd92eb55fb276a337
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-11 00:11:29 +0700

    feat: implement authentication routes and handlers for user registration and login

---
+++ src/routes/auth/auth.handlers.ts
@@ -0,0 +1,118 @@
+import { createDb } from "@/db";
+import { users } from "@/db/schema";
+import type { AppRouteHandler } from "@/lib/types";
+import { LoginRoute, RegisterRoute } from "./auth.routes";
+import { eq } from "drizzle-orm";
+import { hashPassword, verifyPassword } from "@/lib/crypto";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import { verify } from "hono/jwt";
+import { AuthService } from "@/services/auth.service";
+
+export const register: AppRouteHandler<RegisterRoute> = async (c) => {
+  const { db } = createDb(c.env);
+  const { name, email, password } = c.req.valid("json");
+
+  const existingUser = await db.query.users.findFirst({
+    where: eq(users.email, email),
+  });
+  if (existingUser) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          name: "ValidationError",
+          issues: [
+            {
+              code: "user_exists",
+              path: [],
+              message: "User already exists.",
+            },
+          ],
+        },
+      },
+      HttpStatusCodes.UNPROCESSABLE_ENTITY
+    );
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const [newUser] = await db
+    .insert(users)
+    .values({ name, email, password: hashedPassword })
+    .returning();
+
+  return c.json(
+    {
+      name: newUser.name,
+      email: newUser.email,
+    },
+    HttpStatusCodes.OK
+  );
+};
+
+export const login: AppRouteHandler<LoginRoute> = async (c) => {
+  const { db } = createDb(c.env);
+  const { email, password } = c.req.valid("json");
+  const authService = new AuthService(c.env.JWT_SECRET);
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, email),
+  });
+
+  if (!user) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          name: "Auth Error",
+          issues: [
+            {
+              code: "user_not_found",
+              path: [],
+              message: "User not found.",
+            },
+          ],
+        },
+      },
+      HttpStatusCodes.UNAUTHORIZED
+    );
+  }
+
+  const isValidPassword = await verifyPassword(password, user.password);
+  if (!isValidPassword) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          name: "Auth Error",
+          issues: [
+            {
+              code: "cradential_not_valid",
+              path: [],
+              message: "Email Or Password Is Wrong",
+            },
+          ],
+        },
+      },
+      HttpStatusCodes.UNAUTHORIZED
+    );
+  }
+
+  const token = await authService.createToken({
+    userId: user.id.toString(),
+    role: user.role,
+  });
+
+  return c.json(
+    {
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    },
+    HttpStatusCodes.OK
+  );
+};
---
+++ src/routes/auth/auth.routes.ts
@@ -0,0 +1,65 @@
+import { insertUserSchema, selectUserSchema } from "@/db/schema";
+import { createRoute, z } from "@hono/zod-openapi";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import { jsonContent, jsonContentRequired } from "stoker/openapi/helpers";
+import { createErrorSchema } from "stoker/openapi/schemas";
+
+const tags = ["Auth"];
+
+const loginSchema = selectUserSchema.omit({
+  created_at: true,
+  name: true,
+  id: true,
+  role: true,
+});
+
+export const register = createRoute({
+  path: "/auth/register",
+  method: "post",
+  tags,
+  request: {
+    body: jsonContentRequired(insertUserSchema, "The user to create"),
+  },
+  responses: {
+    [HttpStatusCodes.OK]: jsonContent(
+      insertUserSchema.omit({
+        password: true,
+      }),
+      "The created User"
+    ),
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(insertUserSchema),
+      "The validation error(s) | User Already Exists"
+    ),
+  },
+});
+
+export const login = createRoute({
+  path: "/auth/login",
+  method: "post",
+  tags,
+  request: {
+    body: jsonContentRequired(loginSchema, "The user to login"),
+  },
+  responses: {
+    [HttpStatusCodes.OK]: jsonContent(
+      z.object({
+        token: z.string(),
+        user: z.object({
+          id: z.number(),
+          name: z.string(),
+          email: z.string().email(),
+          role: z.string(),
+        }),
+      }),
+      "The logged in User"
+    ),
+    [HttpStatusCodes.UNAUTHORIZED]: jsonContent(
+      createErrorSchema(loginSchema),
+      "The validation error(s) | User Not Found | Invalid Cradentials"
+    ),
+  },
+});
+
+export type RegisterRoute = typeof register;
+export type LoginRoute = typeof login;
