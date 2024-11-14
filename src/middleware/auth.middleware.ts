4708d9751d740907a96bd15947556d2c5100874b
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-14 19:52:23 +0700

    feat: add requireAdminOrOwner middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,6 +2,9 @@ import { Context, Next } from "hono";
 import { AuthService } from "@/services/auth.service";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { MiddlewareHandler } from "hono/types";
+import { createDb } from "@/db";
+import { motivasi } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const authMiddleware: MiddlewareHandler = async (
   c: Context,
@@ -36,5 +39,41 @@ export function requireRole(role: string) {
   };
 }
 
+export function requireAdminOrOwner() {
+  return async function (c: Context, next: Next) {
+    const user = c.get("user");
+    const { db } = createDb(c.env);
+
+    const motivationId = Number(c.req.param("id"));
+
+    if (isNaN(motivationId)) {
+      return c.json(
+        { message: "Invalid motivation ID" },
+        HttpStatusCodes.BAD_REQUEST
+      );
+    }
+
+    const motivations = await db
+      .select()
+      .from(motivasi)
+      .where(eq(motivasi.id, motivationId))
+      .limit(1)
+      .execute();
+
+    const motivation = motivations[0];
+
+    if (!motivation) {
+      return c.json({ message: "Not Found" }, HttpStatusCodes.NOT_FOUND);
+    }
+
+    if (user.role === "ADMIN" || Number(user.userId) === motivation.userId) {
+      await next();
+    } else {
+      return c.json({ message: "Forbidden" }, HttpStatusCodes.FORBIDDEN);
+    }
+  };
+}
+
 export const adminOnly = [authMiddleware, requireRole("ADMIN")];
 export const authenticated = [authMiddleware];
+export const adminOrOwner = [authMiddleware, requireAdminOrOwner()];
